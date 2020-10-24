cc3c17ed4153d38cac461bc447775b9ed52122da
Nick Ingram <wningram2@gmail.com>
2020-10-24 14:39:43 -0700

    Removed test file Added cost calculation implementation

--- src/app.kt
+++ src/app.kt
@@ -1,12 +1,37 @@
-
-fun parse_input(input: String): List<String> {
-	val data = input.replace("\\s".toRegex(), "")
-	val words = data.split(",")
-	
-	return words
+/**
+ 	Validates that the parsed input is in terms of Apples and Oranges.
+ */
+fun validate_input(inputs: List<String>) {
+	for (input in inputs) {
+		when (input.toLowerCase()) {
+			"apple" -> continue
+			"orange" -> continue
+			else -> throw Exception("Invalid input.")
+		}
+	}
 }
 
-fun cost_calculator(input: List<String>): Double {
+/**
+ 	Parses input for a list of products. Throws exception is products not
+ 	given in expected format.
+ */
+fun parse_input(input: String?): List<String> {
+	if (input != null) {
+		val data = input.replace("\\s".toRegex(), "")
+		val words = data.split(",")
+		
+		validate_input(words)
+		return words
+	} else {
+		// If no value was supplied return an empty list
+		return listOf()
+	}
+}
+
+/**
+ 	Calculates the cost of a list of products.
+ */
+fun calculate_cost(input: List<String>): Double {
 	var totalCost = 0.0
 	for (item in input) {
 		when (item.toLowerCase()) {
@@ -28,10 +53,17 @@ fun main(args: Array<String>) {
 		print("Input -> ")
 		val input = readLine()
 		
-		when (input?.toLowerCase()) {
-			"quit" -> run = false
-			"help" -> println(helpText)
-			else -> println("Unrecognized input.")
+		try {
+			when (input?.toLowerCase()) {
+				"quit" -> run = false
+				"help" -> println(helpText)
+				else -> {
+					val cost = calculate_cost(parse_input(input))
+					println("Total Cost is $cost")
+				}
+			}
+		} catch (e: Exception) {
+			println(e.message)
 		}
 	}
 	
